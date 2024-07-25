ac8cb417f57b4a9222085bbcf40aa2ad794ac13a
erodmz <elmer.rodriguez.m@gmail.com>
2024-07-25 02:20:40 -0400

    website synaia ER

---
+++ website/synaia/src/components/forms/form-layouts/index.js
@@ -0,0 +1,23 @@
+import FbOrdinaryForm from './FbOrdinaryForm';
+import FbDefaultForm from './FbDefaultForm';
+import FbBasicHeaderForm from './FbBasicHeaderForm';
+import FbReadonlyForm from './FbReadonlyForm';
+import FbDisabledForm from './FbDisabledForm';
+import FbLeftIconForm from './FbLeftIconForm';
+import FbRightIconForm from './FbRightIconForm';
+import FbInputVariants from './FbInputVariants';
+import FbContactForm from './FbContactForm';
+import FbContact2Form from './FbContact2Form';
+
+export {
+  FbOrdinaryForm,
+  FbDefaultForm,
+  FbBasicHeaderForm,
+  FbReadonlyForm,
+  FbDisabledForm,
+  FbLeftIconForm,
+  FbRightIconForm,
+  FbInputVariants,
+  FbContactForm,
+  FbContact2Form,
+};
