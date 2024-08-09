141b6727fed5bdf85404e48de6b894a3e3997b24
beltrewilton <beltre.wilton@gmail.com>
2024-08-09 12:14:40 -0400

    website has their own project

--- website/synaia/src/components/forms/theme-elements/CustomSelect.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { styled, Select } from '@mui/material';
-
-const CustomSelect = styled((props) => <Select {...props} />)();
-
-export default CustomSelect;
--- welcome_message.sh
+++ welcome_message.sh
@@ -1,5 +1,5 @@
 curl -i -X POST \
   https://graph.facebook.com/v19.0/340502765810581/messages \
-  -H 'Authorization: Bearer EAAN9vS0mBogBOZC7H51FeMnE7DqVZC9uPMLCi7ZCyzBH6qIP7bmPORoJ3mkt3V8CM3mA5Ibq0RM29YtHVaPU5LKYxs0YjGiG10LVNzkJmViQZA7FWuf1v4cF8a0e66oWUA4ZBTysGsNmnSTgLiz8SnIJEZAeFzZApTMf3aZAuIVGtBKIaDUnlTCLZATHp0M6PUUuhBxZBqcgMyTgs6Kz63t0jhAlovPHo70CPeluKTTgfdx7cZD' \
+  -H 'Authorization: Bearer ' \
   -H 'Content-Type: application/json' \
   -d '{ "messaging_product": "whatsapp", "to": "18296456177", "type": "template", "template": { "name": "welcome_message", "language": { "code": "en_US" } } }'
